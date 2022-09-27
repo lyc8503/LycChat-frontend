7c0f7ade55e252e7d09a468bf960b7d0314e3b89
lyc8503 <lyc8503@foxmail.com>
2022-09-27 17:51:44 +0800

    request test & wip

--- src/containers/LoginPage/index.tsx
+++ src/containers/LoginPage/index.tsx
@@ -1,42 +1,75 @@
 import {Button, Form, Input, Space} from "antd";
+import React from "react";
+import axios from "axios";
 
-function LoginPage() {
-  return (
-    <div>
 
-      <h1>
-        Login
-      </h1>
 
-      <Space align="center">
-        <Form>
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[{required: true, message: 'Please input your username!'}]}
-          >
-            <Input/>
-          </Form.Item>
-
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{required: true, message: 'Please input your password!'}]}
-          >
-            <Input.Password/>
-          </Form.Item>
-
-          <Form.Item wrapperCol={{offset: 8, span: 16}}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-
-        </Form>
-      </Space>
-
-    </div>
-  );
+interface PostSessionRequest {
+  username: string,
+  password: string
 }
+
+
+class LoginPage extends React.Component<any, any> {
+
+  onFinish = (values: PostSessionRequest) => {
+    console.log(values);
+
+    axios.post('http://localhost:8080/session', values)
+      .then(function (response) {
+        console.log(response);
+
+      }).catch(function (error) {
+
+    });
+
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>
+          Login
+        </h1>
+
+        <Space align="center">
+          <Form
+            onFinish={this.onFinish}>
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[{required: true, message: 'Please input your username!'}]}
+            >
+              <Input/>
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{required: true, message: 'Please input your password!'}]}
+            >
+              <Input.Password/>
+            </Form.Item>
+
+            <Form.Item>
+              <Space>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+
+                <Button type="primary" htmlType="submit">
+                  Register a new account?
+                </Button>
+              </Space>
+            </Form.Item>
+
+          </Form>
+        </Space>
+
+      </div>
+    );
+  }
+}
+
 
 export default LoginPage;
