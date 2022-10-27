04f2de016bc86c7daa09d1efa0ab31b974fbe514
lyc8503 <36782264+lyc8503@users.noreply.github.com>
2022-10-27 15:12:32 +0800

    use ts instead of anyscript(x) better api request.

--- src/request/index.ts
+++ src/request/index.ts
@@ -1,56 +1,6 @@
-import axios from "axios";
-import {BASE_URL} from "../config";
-import {message} from "antd";
-import _ from "lodash";
+import {AxiosInstance} from "axios";
 
-
-interface ServiceConfig {
-  showErrorOnPage?: boolean
-  showErrorFunc?: Function
+export interface RequestConfig {
+  service: AxiosInstance
 }
 
-function getService(config: ServiceConfig = { showErrorOnPage: true, showErrorFunc: message.error }) {
-  const service = axios.create({
-    baseURL: BASE_URL
-  })
-
-  // 请求拦截器
-  service.interceptors.request.use(config => {
-    console.log(config)
-
-    const token = window.localStorage.getItem('token')
-    _.defaults(config.headers, {'token': token})
-
-    return config
-  }, error => {
-    console.error("请求错误 " + error, error)
-    return Promise.reject(error)
-  })
-
-  // 响应拦截器
-  service.interceptors.response.use(response => {
-    return response.data.data
-  }, function (error) {
-    console.error("请求返回非 2xx " + error, error)
-
-    if (config.showErrorOnPage) {
-      // antd 页面提示
-      if (error.response.data === undefined || error.response.data.msg === undefined) {
-        config.showErrorFunc?.("网络错误, 请稍后再试")
-      } else {
-        config.showErrorFunc?.(error.response.data.msg)
-      }
-    }
-
-    return Promise.reject(error)
-  })
-
-  return service
-}
-
-const service = getService()
-
-// service 是默认情况的 service (在页面上显示错误), 可用 getService 传入自定义参数
-export {
-  service, getService
-}
