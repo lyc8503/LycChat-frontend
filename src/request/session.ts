d0b328dab1cfe80201fce10f4a196db385a83881
lyc8503 <lyc8503@foxmail.com>
2022-10-27 15:12:32 +0800

    use ts instead of anyscript(x) better api request.

---
+++ src/request/session.ts
@@ -0,0 +1,28 @@
+import {RequestConfig} from "./index";
+import {service} from "./service";
+
+
+export interface GetSessionResponse {
+  login: boolean
+  username?: string
+}
+
+export function getSession(config: RequestConfig = { service: service }): Promise<GetSessionResponse> {
+  return config.service.get("/session")
+}
+
+
+export interface PostSessionRequest {
+  password: string;
+  username: string;
+}
+
+export interface PostSessionResponse {
+  expires: number;
+  token: string;
+}
+
+
+export function postSession(request: PostSessionRequest, config: RequestConfig = { service: service }): Promise<PostSessionResponse> {
+  return config.service.post("/session", request)
+}
--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,14 +1,8 @@
 import React, {useEffect, useState} from "react";
 import LoginForm from "./components/Form";
-import {service, getService} from "../../request";
 import {Navigate} from "react-router-dom";
 import {message} from "antd";
-
-
-interface PostSessionRequest {
-  username: string,
-  password: string
-}
+import {getSession, postSession, PostSessionRequest} from "../../request/session";
 
 
 function LoginPage() {
@@ -18,17 +12,18 @@ function LoginPage() {
   // 开始时检查是否已经登录, 如果已经登录则直接跳转主界面
   useEffect(
     () => {
-      getService({showErrorOnPage: false}).get("/session").then(function (response: any) {
-        message.success("已经登录: " + response.username)
-        setIsLoggedIn(response.login)
+      getSession().then(response => {
+        if (response.login) {
+          message.success("已经登录: " + response.username)
+          setIsLoggedIn(response.login)
+        }
       }).catch(() => 0)
     }, []
   )
 
   const onSubmit = (values: PostSessionRequest) => {
-    console.log(values)
 
-    service.post("/session", values)
+    postSession(values)
       .then(function (response: any) {
         window.localStorage.setItem('token', response.token)
         message.success("登录成功")
